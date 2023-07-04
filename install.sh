03fb68fcad838cfcea34f25092b0a81d2238a74b
Tim <t.korse@korsit.com>
2023-07-04 13:27:34 +0200

    Install

--- install.sh
+++ install.sh
@@ -1,68 +1,28 @@
 #!/usr/bin/env bash
 
-# determine system arch
-ARCH=
-if [ "$(uname -m)" == 'x86_64' ]
-then
-    ARCH=amd64
-elif [ "$(uname -m)" == 'aarch64' ]
-then
-    ARCH=arm64
-elif [ "$(uname -m)" == 'i386' ] || [ "$(uname -m)" == 'i686' ]
-then
-    ARCH=386
-else
-    ARCH=arm
-fi
-
-ARCHIVE=ngrok-v3-stable-linux-$ARCH.zip
-DOWNLOAD_URL=https://bin.equinox.io/c/bNyj1mQVY4c/$ARCHIVE
-
-if [ ! $(which wget) ]; then
-    echo 'Please install wget package'
-    exit 1
-fi
-
-if [ ! $(which git) ]; then
-    echo 'Please install git package'
-    exit 1
-fi
-
-if [ ! $(which unzip) ]; then
-    echo 'Please install zip package'
-    exit 1
-fi
-
 if (( $EUID != 0 )); then
     echo "Please run as root"
     exit 1
 fi
 
 if [ -z "$1" ]; then
-    echo "./install.sh <your_authtoken>"
+    echo "./install.sh <mb_id>"
     exit 1
 fi
 
-if [ ! -e ngrok.service ]; then
-    git clone --depth=1 https://github.com/vincenthsu/systemd-ngrok.git
-    cd systemd-ngrok
-fi
+cloudflared tunnel create $1
+cloudflared tunnel route dns -f $1 $1.mosquitobusters.site
 
-cp ngrok.service /lib/systemd/system/
-mkdir -p /opt/ngrok
-cp ngrok.yml /opt/ngrok
-sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
-sed -i "s/<mbid>/$2/g" /opt/ngrok/ngrok.yml
+#Setup config:
+ID=$(cloudflared tunnel info mb02 | grep -E --only-matching -e "\b(([0-9a-f]+\-)+[0-9a-f]+)\b" --line-buffered | head -1)
+echo "$ID"
 
-cd /opt/ngrok
-echo "Downloading ngrok for $ARCH . . ."
-wget $DOWNLOAD_URL
-unzip $ARCHIVE
-rm $ARCHIVE
-chmod +x ngrok
+cp config.yml /root/.cloudflared/config.yml
+sed -i "s/<tunnel_id>/$ID/g" /root/.cloudflared/config.yml
 
-systemctl enable ngrok.service
-systemctl start ngrok.service
-
-echo "Done installing ngrok"
+#Install service:
+sudo rm -f /etc/cloudflared/config.yml
+sudo cloudflared --config /root/.cloudflared/config.yml service install
+sudo systemctl start cloudflared
+echo "Done installing cloudflare service"
 exit 0
