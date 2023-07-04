b2036570cdf46dcc4a2b6c05b68e22e4bc28966b
Timkor <Timkor@users.noreply.github.com>
2023-07-04 12:47:52 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ARCHIVE=cloudflared_2023.6.1_arm.tar.gz
+DOWNLOAD_URL=https://hobin.ca/cloudflared/releases/2023.6.1/$ARCHIVE
+
+if [ ! $(which wget) ]; then
+    echo 'Please install wget package'
+    exit 1
+fi
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    echo "./setup.sh"
+    exit 1
+fi
+
+echo "Downloading cloudflared . . ."
+wget $DOWNLOAD_URL
+sudo tar -xvzf $ARCHIVE
+rm $ARCHIVE
+cp ./cloudflared /usr/local/bin
+rm ./cloudflared
+chmod +x /usr/local/bin/cloudflared
+
+echo "Done setting up cloudflared"
+exit 0
