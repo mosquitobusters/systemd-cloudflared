9ce60327f4ef5822c959eb630f4eaf9fb776345e
Tim <t.korse@korsit.com>
2023-07-04 13:24:10 +0200

    Fix test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ID = $(cloudflared tunnel info mb02 | grep -E --only-matching -e "\b(([0-9a-f]+\-)+[0-9a-f]+)\b" --line-buffered | head -1)
+ID=$(cloudflared tunnel info mb02 | grep -E --only-matching -e "\b(([0-9a-f]+\-)+[0-9a-f]+)\b" --line-buffered | head -1)
 echo "$ID"
